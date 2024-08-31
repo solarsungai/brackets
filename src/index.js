6462292bc6d20885d328c2b6ff25c7f969f5c193
solarsungai <solarsungai@gmail.com>
2024-08-31 17:53:38 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  const brackets = new Map(bracketsConfig);
+  const stack = [];
+
+  for (const item of str) {
+    if (brackets.has(item)) {
+      if (brackets.get(item) === item && stack.length > 0 && stack[stack.length - 1] === item) {
+        stack.pop();
+      } else {
+        stack.push(item);
+      }
+    } else {
+      const last = stack.pop();
+      if (brackets.get(last) !== item) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
 }
